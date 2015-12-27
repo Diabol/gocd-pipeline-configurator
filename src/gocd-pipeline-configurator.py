c2568309820b4bbf8f74edad51c89bac840537d3
Dennis Granath <dennis@diabol.se>
2015-12-27 21:21:40 +0100

    Add config commandline option.

--- src/gocd-pipeline-configurator.py
+++ src/gocd-pipeline-configurator.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-import json, yaml, ConfigParser, os, json, requests, sys
+import json, yaml, ConfigParser, os, json, requests, sys, getopt
 
 CONFIG_SECTION = "default"
 BASE_URL_OPTION_NAME  = "gocd_base_url"
 PIPELINE_DIR_OPTION_NAME = "pipeline_dir"
 options = {}
 
-def parseConfigFile():
+def parseConfigFile(path):
     config = ConfigParser.ConfigParser()
 
-    config.read(os.getcwd() + '/.config.ini')
+    config.read(path)
     for section in config.sections():
         if section == CONFIG_SECTION:
             for option in config.options(section):
@@ -21,14 +21,37 @@ def createPipeline(file_name):
     file_path = options[PIPELINE_DIR_OPTION_NAME] + "/" + file_name
     yaml_data = yaml.load(open(file_path))
     json_data = json.dumps(yaml_data)
-    print json_data
     headers = {'Accept': 'application/vnd.go.cd.v1+json', 'Content-Type': 'application/json'}
     response = requests.post(create_url, data=json_data, auth=(options['api_username'], options['api_password']), headers=headers)
     print response
 
+
+def getDefaultConfig():
+    working_dir_conf = os.getcwd() + "/.config.ini"
+    if os.path.isfile(working_dir_conf):
+        return working_dir_conf
+    return None
+
 #Main
 def main(argv):
-    parseConfigFile()
+    configfile = getDefaultConfig()
+    try:
+      opts, args = getopt.getopt(argv,"hc:",["config="])
+    except getopt.GetoptError:
+      print 'gocd-pipeline-configurator.py -c <configfile>'
+      sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print 'gocd-pipeline-configurator.py -c <configfile>'
+            sys.exit()
+        elif opt in ("-c", "--config"):
+            configfile = arg
+
+    if not os.path.isfile(configfile):
+        print "This configfile does not exist: " + configfile
+        sys.exit(1)
+
+    parseConfigFile(configfile)
     for file_name in os.listdir(options['pipeline_dir']):
         createPipeline(file_name)
 
