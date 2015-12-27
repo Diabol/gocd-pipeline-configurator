be49e50a06c374245eb6331c0ef49497ca3acca9
Dennis Granath <dennis@diabol.se>
2015-12-27 23:32:48 +0100

    Create pipeline if not exist or edit if exists

--- src/gocd-pipeline-configurator.py
+++ src/gocd-pipeline-configurator.py
@@ -15,15 +15,37 @@ def parseConfigFile(path):
             for option in config.options(section):
                 options[option] = config.get(section, option)
 
+def getEtag(pipeline_name):
+    get_url = options[BASE_URL_OPTION_NAME] + "/go/api/admin/pipelines/" + pipeline_name
+    headers = {'Accept': 'application/vnd.go.cd.v1+json'}
+    response = requests.get(get_url, auth=(options['api_username'], options['api_password']), headers=headers)
+    if response.status_code == 200:
+        return response.headers['etag']
+    return None
 
-def createPipeline(file_name):
-    create_url = options[BASE_URL_OPTION_NAME] + "/go/api/admin/pipelines"
+def createOrEditPipeline(file_name):
     file_path = options[PIPELINE_DIR_OPTION_NAME] + "/" + file_name
+    yaml_data = yaml.load(open(file_path))
+    pipeline_name = yaml_data['pipeline']['name']
+    etag = getEtag(pipeline_name)
+    if  etag == None:
+        createPipeline(file_path)
+    else:
+        editPipeline(file_path, pipeline_name, etag)
+
+def editPipeline(file_path, pipeline_name, etag):
+    yaml_data = yaml.load(open(file_path))
+    json_data = json.dumps(yaml_data)
+    edit_url = options[BASE_URL_OPTION_NAME] + "/go/api/admin/pipelines/" + pipeline_name
+    headers = {'Accept': 'application/vnd.go.cd.v1+json', 'Content-Type': 'application/json', "If-Match": etag}
+    response = requests.put(edit_url, data=json_data, auth=(options['api_username'], options['api_password']), headers=headers)
+
+
+def createPipeline(file_path):
     yaml_data = yaml.load(open(file_path))
     json_data = json.dumps(yaml_data)
     headers = {'Accept': 'application/vnd.go.cd.v1+json', 'Content-Type': 'application/json'}
     response = requests.post(create_url, data=json_data, auth=(options['api_username'], options['api_password']), headers=headers)
-    print response
 
 
 def getDefaultConfig():
@@ -53,7 +75,7 @@ def main(argv):
 
     parseConfigFile(configfile)
     for file_name in os.listdir(options['pipeline_dir']):
-        createPipeline(file_name)
+        createOrEditPipeline(file_name)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
